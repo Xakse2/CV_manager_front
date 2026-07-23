8991bcbdaddec69e73d8ec63675fae2e81223e88
Xakse Xakse <roma.roma.romanov2001@gmail.com>
2026-07-23 13:13:51 +0300

    fix: add api url

--- src/store/baseQuery.ts
+++ src/store/baseQuery.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "./store";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000/api",
+  baseUrl: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
   credentials: "include",
 
   prepareHeaders: (headers, { getState }) => {
